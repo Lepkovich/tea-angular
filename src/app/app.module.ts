8a6e3153dd072b0d4239eb36ba5a2abf5468c5f7
Lepkovich <6332133@gmail.com>
2023-11-07 17:03:14 +0300

    started from work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { MainComponent } from './components/pages/main/main.component';
+import { ProductsComponent } from './components/pages/products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    ProductsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/scripts/script.js
@@ -0,0 +1,33 @@
+
+$('.popup-link').magnificPopup({
+    type: 'image'
+});
+
+$(document).ready(function() {
+
+    $('#inputZip').keydown(function (e) { // проверяем поле Индекс на вводимые символы
+        let number = parseInt(e.key); // мы приводим вводимые символы к цифрам. все, кроме цифр дадут NaN
+        if (isNaN(number)) { // любая нецифра (NaN) будет true
+            return false; // и мы выйдем из функции (не цифры не введутся)
+        }
+
+    });
+
+    $('#btnSend').bind('click', function (e) {
+        e.preventDefault();
+        for (let a = 1; a < $('input').length; a++) { // первый input не трогаем (он в шапке сайта
+            if ($('input').eq(a).val() < 1) {  // если в поле нет ни одного символа, выведем alert
+                alert('Заполните все поля');
+                return false;
+            }
+            if ($('#inputZip').val().length !==6) {
+                alert('В поле Индекс должно быть 6 цифр');
+                return false;
+            }
+        }
+        $('#makeOrder').addClass("hideElement");
+        $('#thankYou').removeClass("hideElement");
+
+    });
+});
+
